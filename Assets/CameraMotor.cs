876216da508d670f0f3e44cc71f4b2964030fe85
mousedesvio <redbrickcar2@gmail.com>
2022-04-05 00:58:23 -0300

    att files

---
+++ Assets/CameraMotor.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;
+    public float boundX = 0.4f;
+    public float boundY = 0.2f;
+
+    public Vector2 Min, Max;
+
+    private void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+
+        Vector3 delta = Vector3.zero;
+
+        float deltaX = Mathf.Clamp(lookAt.position.x, Min.x, Max.x) - transform.position.x;
+        if(deltaX > boundX || deltaX < -boundX) 
+        {
+            if (transform.position.x < Mathf.Clamp(lookAt.position.x, Min.x, Max.x))
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        float deltaY = Mathf.Clamp(lookAt.position.y, Min.y, Max.y) - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < Mathf.Clamp(lookAt.position.y, Min.y, Max.y))
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        transform.position += new Vector3(
+            delta.x,
+            delta.y,
+            0
+            );
+
+    }
+}
